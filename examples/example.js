8eaf5d8f582ecddc61822c20dbea742d4a7506c2
hayper <hayper1919@gmail.com>
2020-11-20 11:51:15 +0700

    damn you runkit

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,7 @@
 const genshin = require("genshin-impact-wrapper");
 
 (async () => {
+    /* I don't know why but runkit won't work with this :OMEGALUL: */
     console.log((await genshin.getCharacters()));
     console.log((await genshin.getCharacterByRarity(5)));
     console.log((await genshin.getCharacterByName("Diona")));
