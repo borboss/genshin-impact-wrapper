22fef65fc06c2717e0562f0c30b24e28974e4e6b
hayper <hayper1919@gmail.com>
2020-11-20 11:48:30 +0700

    Added JSDoc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import * as dataObject from "./util/dataObject";
 import * as type from "./util/types";
 import * as glob from "glob";
 
+
+/**
+ * @description Return all of the character data that this library have.
+ */
+
 export async function getCharacters(): Promise<dataObject.character[]> {
     let characterDataList: dataObject.character[] = [];
     const filePathList = glob.sync(__dirname + "\\..\\data\\characters\\**\\*.json");
@@ -11,25 +16,49 @@ export async function getCharacters(): Promise<dataObject.character[]> {
     return characterDataList;
 }
 
-export async function getCharacterByName(name:string): Promise<dataObject.character[]> {
-    return (await getCharacters()).filter((character) => character.name.toLowerCase() === name.toLowerCase());
+/**
+ * @description Get the character data by their name.
+ */
+
+export async function getCharacterByName(name:string): Promise<dataObject.character> {
+    return (await getCharacters()).filter((character) => character.name.toLowerCase() === name.toLowerCase())[0];
 }
+
+/**
+ * @description Get a list of character data by their rarity.
+ */
 
 export async function getCharacterByRarity(rarity:number): Promise<dataObject.character[]> {
     return (await getCharacters()).filter((character) => character.rarity === rarity);
 }
 
+/**
+ * @description Get a list of character data by their gender.
+ */
+
 export async function getCharacterByGender(gender:type.gender): Promise<dataObject.character[]> {
     return (await getCharacters()).filter((character) => character.gender === gender);
 }
+
+/**
+ * @description Get a list of character data by their weapon.
+ */
 
 export async function getCharacterByWeapon(weapon:type.weapon): Promise<dataObject.character[]> {
     return (await getCharacters()).filter((character) => character.weapon === weapon);
 }
 
+/**
+ * @description Get a list of character data by their vision.
+ */
+
 export async function getCharacterByVision(vision:type.vision): Promise<dataObject.character[]> {
     return (await getCharacters()).filter((character) => character.vision === vision);
 }
+
+/**
+ * @description Get the character data by their nation.
+ */
 
 export async function getCharacterByNation(nation:type.nation): Promise<dataObject.character[]> {
     return (await getCharacters()).filter((character) => character.nation === nation);
